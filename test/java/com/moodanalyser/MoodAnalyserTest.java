26365d231513c280c6f0db01153ed7fecb03b64a
maddimsetti <82135298+maddimsetti@users.noreply.github.com>
2021-05-12 11:59:08 +0530

    Modified the code as per Naming Convention

--- test/java/com/moodanalyser/MoodAnalyserTest.java
+++ test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -5,13 +5,13 @@ import org.junit.jupiter.api.Test;
 
 public class MoodAnalyserTest {
     @Test
-    public void givenMessage_WhenSad_ShouldReturnFalse() {
+    public void givenMessage_WhenSad_ShouldReturnSad() {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
         String mood = moodAnalyser.analyseMood("I am in Sad Mood");
         Assertions.assertEquals("SAD",mood);
     }
     @Test
-    public void givenMessage_WhenHappy_ShouldReturnTrue() {
+    public void givenMessage_WhenHappy_ShouldReturnHappy() {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
         String mood = moodAnalyser.analyseMood("Iam in Happy Mood");
         Assertions.assertEquals("HAPPY", mood);
